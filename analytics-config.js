e811bfb2e8a93666ec57d8be0f1769d0cc74a02e
Kuba Mierosławski <seamonk6616@gmail.com>
2026-08-06 00:47:52 +0200

    Enable BonsAi Studio Google Analytics

--- analytics-config.js
+++ analytics-config.js
@@ -1,3 +1,3 @@
 // Wpisz identyfikator usługi GA4 z konta BonsAi Studio, np. "G-XXXXXXXXXX".
 // Nie używaj identyfikatora innej marki lub domeny.
-window.ITBONSAI_GA4_ID = '';
+window.ITBONSAI_GA4_ID = 'G-SPBZV7WDPN';
