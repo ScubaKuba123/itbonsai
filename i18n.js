4e4b28dbb0b2d3eab4f7ed4c4389ffe99ccfccdc
Kuba Mierosławski <seamonk6616@gmail.com>
2026-08-12 10:03:16 +0200

    Restructure BonsAI business product ecosystem

--- i18n.js
+++ i18n.js
@@ -495,7 +495,7 @@
   }
 
   try {
-    const businessTranslations = await fetch("dla-biznesu-translations.json?v=8", {
+    const businessTranslations = await fetch("dla-biznesu-translations.json?v=9", {
       cache: "force-cache",
     }).then((response) => (response.ok ? response.json() : null));
     if (businessTranslations) {
