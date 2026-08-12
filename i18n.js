686380a86a12396693b5d311c152e6702fbae311
Kuba Mierosławski <seamonk6616@gmail.com>
2026-08-12 10:10:13 +0200

    Preserve complete BonsAI product descriptions

--- i18n.js
+++ i18n.js
@@ -495,7 +495,7 @@
   }
 
   try {
-    const businessTranslations = await fetch("dla-biznesu-translations.json?v=9", {
+    const businessTranslations = await fetch("dla-biznesu-translations.json?v=10", {
       cache: "force-cache",
     }).then((response) => (response.ok ? response.json() : null));
     if (businessTranslations) {
